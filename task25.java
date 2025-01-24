e00987d18edd4b476c100638450361b6c3b5f2b4
KusKusVenom <berkanat07@gmail.com>
2025-01-24 08:28:22 +0500

    Create task25.java

---
+++ task25.java
@@ -0,0 +1,15 @@
+package assignment3_1;
+
+public class t6 {
+    private String color = "BLACK";
+    private int year = 2020;
+    private String body = "Coupe";
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
