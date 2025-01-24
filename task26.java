e3f89e7a772c52ee5cc60092c23bfb9c9073edd1
KusKusVenom <berkanat07@gmail.com>
2025-01-24 08:29:15 +0500

    Create task26.java

---
+++ task26.java
@@ -0,0 +1,15 @@
+package assignment3_1;
+
+public class t7 {
+    private int salary = 1000;
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        if (salary > this.salary) {
+            this.salary = salary;
+        }
+    }
+}
