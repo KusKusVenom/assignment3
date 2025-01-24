61f18c978ccf690643285e0eee6dac535ba76a85
KusKusVenom <berkanat07@gmail.com>
2025-01-24 08:24:45 +0500

    Create task19.java

---
+++ task19.java
@@ -0,0 +1,11 @@
+package assignment3_1;
+
+public class t1 {
+    public t1() {
+        System.out.println("Skyscraper built.");
+    }
+
+    public static void main(String[] args) {
+        new t1();
+    }
+}
