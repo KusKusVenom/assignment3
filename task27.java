9d04db6433317762ef03d6893552f500c631ab6e
KusKusVenom <berkanat07@gmail.com>
2025-01-24 08:29:42 +0500

    Create task27.java

---
+++ task27.java
@@ -0,0 +1,11 @@
+package assignment3_1;
+
+public class t8 {
+    public static void main(String[] args) {
+        String s1 = "JavaENU"; // Uncommented this line
+        String s2 = "JavaENU";
+        String s3 = "JavaENU";
+        System.out.println(s1 == s3);
+        System.out.println(s2.equals(s3));
+    }
+}
